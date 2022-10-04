44a01fef6ce7f0a0aa4d2e1b5d1a6d9e20080e7f
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-04 23:31:36 +0700

    finish activity lab04_b

--- App_lab04b_GridView/App.js
+++ App_lab04b_GridView/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import ComponentGirdView from './src/components/ComponentGirdView';
+import ComponentItem from './src/components/ComponentItem';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ComponentGirdView />
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
